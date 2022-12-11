e9d5339033645f05bafa13f84745ef3e66adb698
Suphawat Sujai <suphawat_2544_196@hotmail.com>
2022-12-11 17:06:19 +0700

    Update create_file.js

--- Nodejs_basic/create_file.js
+++ Nodejs_basic/create_file.js
@@ -1,16 +1,22 @@
 const url1 ="file.dev/file.js"
+const url2 ="happy.dev/file.js"
+const url3 ="resident.dev/file.js"
 
 function downloading(url,callback){
     //console.log(`Downloadin = ${url}`)
     console.log(`Downloading from ${url}`)
     setTimeout(()=>{
         
-        callback()
-    },5000)
+        callback(url)
+    },2000)
 }
 
-function complete(){
-    console.log("complete download")
-}
-
-downloading(url1,complete)
+downloading(url1,function(result){
+    console.log(`Download ${result} completed`)
+    downloading(url2,function(result){
+        console.log(`Download ${result} completed`)
+        downloading(url3,function(result){
+            console.log(`Download ${result} completed`)
+        })
+    })
+})
