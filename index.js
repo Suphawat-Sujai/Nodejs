371e8d72f515bc94a8ccac05142d1ebccaca8df1
Suphawat Sujai <suphawat_2544_196@hotmail.com>
2022-12-12 22:42:11 +0700

    create nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const server = http.createServer(function (req, res){
     const myhtml =`<h1>Hello world</h1>
-    <p>By suphawat sujai</p>`
+    <p style = "color:yellow" >By suphawat sujai</p>`
 
     res.write(myhtml)
     res.end()
