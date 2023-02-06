7776dfb6bc55087a088f80d6d16a7a05863cf221
Suphawat Sujai <suphawat_2544_196@hotmail.com>
2023-02-06 22:18:49 +0700

    เปลี่ยน location index

--- index_express.js
+++ index_express.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const app = express() //ใช้งาน express
+// const express = require('express')
+// const app = express() //ใช้งาน express
 
-app.use((req,res) =>{
-    res.send("Hello Express.js")
-})
+// app.use((req,res) =>{
+//     res.send("Hello world")
+// })
 
-app.listen(8080,()=>{//กำหนดพอต
-    console.log("listening on at port 8080")
+// app.listen(8080,() =>{//กำหนดพอต
+//     console.log("listening on at port 8080")
 
-
-})
+// })
