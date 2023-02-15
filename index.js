46c82cc28a6108e89b3d8afdd5ae9341450489ba
Suphawat Sujai <suphawat_2544_196@hotmail.com>
2023-02-15 17:48:20 +0700

    อัพเดท server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //ใช้าำหรับสั่ง run server ขึ้นมา
 const express = require('express')
-const router = require('./Router/my_router')
+const path = require('path')
 const app = express() //ใช้งาน express
+app.use(express.static(path.join(__dirname,'public')))
 
-app.use(router) //ทำหน้าที่อื่น เเต่เปลั้นยนรเเปลงคำสั่งใช้ router เเทน
 
 app.listen(8080,() =>{//กำหนดพอต
     console.log("listening on at port 8080")
